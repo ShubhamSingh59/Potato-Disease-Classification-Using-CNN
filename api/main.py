2e57c014fa3da6eeb8a67ac937ca4d4237fff340
Shubham Singh <shubhams@iitgn.ac.in>
2025-05-14 07:58:42 +0530

    Web

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
@@ -7,8 +8,20 @@ import tensorflow as tf
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 MODEL = tf.keras.models.load_model("../models/2.keras")
-CLASS_NAME = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+CLASS_NAME = ['Early_blight', 'Late_blight', 'Healthy']
 
 @app.get('/ping')
 async def ping():
@@ -21,16 +34,14 @@ def read_file_as_image(data)->np.ndarray:
 @app.post('/predict')
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
-    # we need to chnge this contents in to numpy array
     image = read_file_as_image(contents)
-    image_batch = np.expand_dims(image,0)
-    index = np.argmax(MODEL.predict(image_batch))
-    confidense = 100*np.max(MODEL.predict(image_batch))
-    pred_class = CLASS_NAME[index]
-    print(index)
+    image_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(image_batch)
+    confidence = float(np.max(predictions))  
+    class_index = np.argmax(predictions)
     return {
-        "Class" : pred_class,
-        "Confidence": confidense
+        "class": CLASS_NAME[class_index],  
+        "confidence": confidence  
     }
 
 
